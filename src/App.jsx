d969bbc8f7a29bbe922ad8e5a1c44a73507552a3
ishahasan <isha@menahomes.com>
2025-11-25 18:53:24 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -99,7 +99,7 @@ const [song, setSong] = useState('')
     try {
       // POST request to the API with song and artist
       const response = await fetch('https://soundfacts-endpoint.vercel.app/api/soundfacts', {
-        method: 'POST',
+        method: 'OPTIONS',
         headers: {
           'Content-Type': 'application/json'
         },
